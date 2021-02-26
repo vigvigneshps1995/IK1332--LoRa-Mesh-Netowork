f360d14b836a0c8fb626aa17199c2eba7132d8a8
Vignesh PS <vigneshps@Vigneshs-MacBook-Pro.local>
2021-02-26 14:35:36 +0100

    pico network implemetation

--- LoraClient.py
+++ LoraClient.py
@@ -1,68 +1,69 @@
 import time
 import json
 import PyLora
-
-
-# LoRA operating constants
-LORA_FREQ = 866 * 1000000
-LORA_BW = 125 * 1000
-LORA_SF = 8
-LORA_CR = 8
-LORA_RECEIVE_TIMEOUT = 5         # in seconds
-
-
-
+from multiprocessing import Process, Queue
 
 
 class LoraClient:
 
-    def __init__(self, freq=LORA_FREQ, bw=LORA_BW, sf=LORA_SF, cr=LORA_CR, enable_crc=True):
+    # LoRA operating constants
+    LORA_FREQ = 866 * 1000000
+    LORA_BW = 125 * 1000
+    LORA_SF = 8
+    LORA_CR = 8
+
+    def __init__(self, send_q, recv_q):
         self.lora_client = PyLora
         self.lora_client.init()
-        self.lora_client.set_frequency(freq)
-        self.lora_client.set_bandwidth(bw)
-        self.lora_client.set_coding_rate(cr)
-        self.lora_client.set_spreading_factor(sf)
-        if enable_crc:
-            self.lora_client.enable_crc()
-        else:
-            self.lora_client.disable_crc()
+        self.lora_client.set_frequency(LoraClient_MP.LORA_FREQ)
+        self.lora_client.set_bandwidth(LoraClient_MP.LORA_BW)
+        self.lora_client.set_coding_rate(LoraClient_MP.LORA_CR)
+        self.lora_client.set_spreading_factor(LoraClient_MP.LORA_SF)
+        self.lora_client.enable_crc()
 
-    def send_payload(self, payload):
-        try:
-            # print ("Sending payload: ")
-            # print (json.dumps(payload, indent=2))
-            self.lora_client.send_packet(json.dumps(payload))
-        except Exception as e:
-            print ("Error sending message")
-            print (str(e))
+        # queues
+        self.send_queue = send_q
+        self.recv_queue = recv_q
 
-    def receive_payload(self, timeout=LORA_RECEIVE_TIMEOUT):
-        payload = None
-        t = time.time()
-        try:
-            self.lora_client.receive()      # put into receive mode
-            while True:
+    def start_client(self):
+        self.lora_client.receive()      # always keep the lora in recevier mode
+        while True:
+            # send a packet if the packet is available
+            try:
+                while not self.send_queue.empty():
+                    payload = self.send_queue.get()
+                    #print ("Sending payload: ")
+                    #print (json.dumps(payload, indent=2))
+                    self.lora_client.send_packet(json.dumps(payload))
+                    time.sleep(1)
+            except Exception as e:
+                print ("Error sending message")
+                print (str(e))
+            finally:
+                self.lora_client.receive()      # put into receive mode
+
+            # get a packet if it is available
+            try:
                 if self.lora_client.packet_available():
-                    payload = self.lora_client.receive_packet()
-                    break
-                elif time.time() - t > timeout:
-                    raise Exception("ReceiveTimeout")
-                time.sleep(0.1)
-        except Exception as e:
-            if str(e) != "ReceiveTimeout":
+                    payload = json.loads(str(self.lora_client.receive_packet()))
+                    self.recv_queue.put(payload)
+                    #print ("Payload Received: ")
+                    #print (json.dumps(payload, indent=2))
+                    time.sleep(0.5)
+            except Exception as e:
                 print ("Error receiving message")
                 print (str(e))
-        else:
-            payload = json.loads(str(payload))
-            # print ("Payload Received: ")
-            # print (json.dumps(payload, indent=2))
-        return payload
 
 
 if __name__ == "__main__":
-    from LoraClient import LoraClient
-    c = LoraClient()
-    while True:
-        c.send_payload("hello")
+
+    send_q = Queue()
+    recv_q = Queue()
+    c = LoraClient_MP(send_q, recv_q)
+
+    p = Process(target=c.start_client)
+    p.start()
+    while (True):
+        send_q.put({"hello": "hi"})
         time.sleep(1)
+    p.join()
---
+++ PicoNetClient.py
@@ -0,0 +1,98 @@
+import time
+import json
+import copy
+import argparse
+import datetime
+from threading import Thread
+from multiprocessing import Process, Queue
+from LoraClient import LoraClient
+
+
+# this is the format of message that is send across all the nodes
+PAYLOAD_FORMAT = {
+    "src": None,                 # src device id
+    "dst": None,                 # dst device id
+    "payload": None,             # payload
+}
+
+
+def print_payload(payload):
+    print ("\tMessage: %s -> %s\t\t%s\n" % (payload['src'], payload['dst'], payload['payload']))
+
+
+def sender_thread(node_id, gw_id, send_q, recv_q, mode):
+    while True:
+        # format message to master client
+        print ("Sending to gateway %s" % gw_id)
+        send_msg = copy.deepcopy(PAYLOAD_FORMAT)
+        send_msg["src"] = node_id
+        send_msg["dst"] = gw_id
+        send_msg["payload"] = "%s : Hello from %s " % (str(datetime.datetime.now()), node_id)
+        print_payload(send_msg)
+        send_q.put(send_msg)
+        # sleep for 30 seconds for next update
+        time.sleep(10)
+
+
+def receiver_thread(node_id, gw_id, send_q, recv_q, mode):
+    while True:
+        # receive a message
+        recv_msg = None
+        if not recv_q.empty():
+            recv_msg = recv_q.get()
+        if recv_msg:
+            if mode == "MASTER":
+                print ("Message Received from %s" % recv_msg['src'])
+                print_payload(recv_msg)
+            if mode == "BRIDGE":
+                print ("Bridging from %s to %s" % (recv_msg['src'], gw_id))
+                recv_msg['src'] = node_id
+                recv_msg['dst'] = gw_id
+                print_payload(recv_msg)
+                send_q.put(recv_msg)
+
+
+if __name__ == "__main__":
+
+    # command line arguments
+    parser = argparse.ArgumentParser(description='LoRa Mesh Client')
+    parser.add_argument('--mode', required=True, help="The operating mode of the client. SLAVE | MASTER | BRIDGE ")
+    parser.add_argument('--node-id', required=True, help="id assigned to node")
+    parser.add_argument('--gateway-id', required=True, help="id of the gateway node")
+    args = parser.parse_args()
+
+    # operating parameters
+    node_id = args.node_id
+    gw_id = args.gateway_id
+    mode = args.mode
+
+    # start the lora client process
+    send_q, recv_q = Queue(), Queue()
+    lora_client = LoraClient(send_q, recv_q)
+    p = Process(target=lora_client.start_client)
+    p.start()
+
+    # start threads
+    if mode == "SLAVE":
+        print ("Starting mesh client in %s MODE" % mode)
+        thread1 = Thread(target=sender_thread, args=(node_id, gw_id, send_q, recv_q, mode))
+        thread1.start()
+        thread1.join()
+        p.join()
+
+    if mode == "MASTER":
+        print ("Starting mesh client in %s MODE" % mode)
+        thread1 = Thread(target=receiver_thread, args=(node_id, gw_id, send_q, recv_q, mode))
+        thread1.start()
+        thread1.join()
+        p.join()
+
+    if mode == "BRIDGE":
+        print ("Starting mesh client in %s MODE" % mode)
+        thread1 = Thread(target=sender_thread, args=(node_id, gw_id, send_q, recv_q, mode))
+        thread2 = Thread(target=receiver_thread, args=(node_id, gw_id, send_q, recv_q, mode))
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
+        p.join()
